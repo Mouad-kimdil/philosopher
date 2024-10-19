63755bca95d63e1fccc8fc6fdfa9931fbf5cb9f6
Mouad Kimdil <71198294+Mouad-kimdil@users.noreply.github.com>
2024-10-19 20:26:21 +0000

    remove unused function

--- philo/philo.c
+++ philo/philo.c
@@ -49,28 +49,6 @@ int	start_philo(t_arg *args, t_philo *ph)
 	return (0);
 }
 
-void	print_args(t_philo ph)
-{
-	printf("number of philos: %d\t", ph.args->num_of_philos);
-	printf("time to die: %zu\t", ph.args->time_to_die);
-	printf("time to eat: %zu\t", ph.args->time_to_eat);
-	printf("time to sleep: %zu\t", ph.args->time_to_sleep);
-	printf("number of times to eat: %d\t", ph.args->num_times_to_eat);
-	printf("thread: %p\t", &ph.thread);
-	printf("philo id: %d\t", ph.philo_id);
-	printf("eating: %d\t", ph.eating);
-	printf("dead: %d\t", *ph.dead);
-	printf("meals: %d\t", ph.meals);
-	printf("end lock: %p\t", ph.end_lock);
-	printf("message: %p\t", ph.message);
-	printf("meal: %p\t", ph.meal);
-	printf("left fork: %p\t", ph.l_fork);
-	printf("right fork: %p\t", ph.r_fork);
-	printf("start time: %zu\t", ph.start_time);
-	printf("last meal time: %zu\t", ph.last_meal_time);
-	printf("args: %p\n", ph.args);
-}
-
 int main(int ac, char **av)
 {
 	t_mutex			mutex;
